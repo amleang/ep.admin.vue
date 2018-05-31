d9503521e40e168751a081b5ee50dc2323dc0ba8
少游[赵安良] <shaoyou@jzez100.com>
2018-05-31 21:02:03 +0800

    update

--- src/components/base/admin.vue
+++ src/components/base/admin.vue
@@ -1,56 +1,56 @@
 <template>
-    <div class="main">
-        <div v-show="isMenu" class="nav-let">
-            <div class="nav-top">
-                后台管理系统
-                <div class="line"></div>
-            </div>
-            <el-menu style="height:100%;" @open="menu_open_handle" @close="menu_close_handle"  background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
-                <el-submenu index="1">
-                    <template slot="title">
-                        <i class="el-icon-location"></i>
-                        <span>导航一</span>
-                    </template>
-                    <el-menu-item-group>
-                        <template slot="title">分组一</template>
-                        <el-menu-item index="1-1">选项1</el-menu-item>
-                        <el-menu-item index="1-2">选项2</el-menu-item>
-                    </el-menu-item-group>
-                    <el-menu-item-group title="分组2">
-                        <el-menu-item index="1-3">选项3</el-menu-item>
-                    </el-menu-item-group>
-                    <el-submenu index="1-4">
-                        <template slot="title">选项4</template>
-                        <el-menu-item index="1-4-1">选项1</el-menu-item>
-                    </el-submenu>
-                </el-submenu>
-                <el-menu-item index="2">
-                    <i class="el-icon-menu"></i>
-                    <span slot="title">导航二</span>
-                </el-menu-item>
-                <el-menu-item index="3" disabled>
-                    <i class="el-icon-document"></i>
-                    <span slot="title">导航三</span>
-                </el-menu-item>
-                <el-menu-item index="4">
-                    <i class="el-icon-setting"></i>
-                    <span slot="title">导航四</span>
-                </el-menu-item>
-            </el-menu>
-        </div>
-        <div class="content">
-            <div class="sub-main" :style="`margin-left:${!isMenu?'0px':'240px'}`">
-                <div class="nav-top">
-                   <div class="change-menu" @click="isMenu=!isMenu">
-                       <i class="el-icon-menu"></i>
-                   </div>
-                </div>
-                <div class="con">
-                    content
-                </div>
-            </div>
-        </div>
+  <div class="main">
+    <div v-show="isMenu" class="nav-let">
+      <div class="nav-top">
+        后台管理系统
+        <div class="line"></div>
+      </div>
+      <el-menu style="height:100%;" @open="menu_open_handle" @close="menu_close_handle" background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
+        <el-submenu index="1">
+          <template slot="title">
+            <i class="el-icon-location"></i>
+            <span>导航一</span>
+          </template>
+          <el-menu-item-group>
+            <template slot="title">分组一</template>
+            <el-menu-item index="1-1">选项1</el-menu-item>
+            <el-menu-item index="1-2">选项2</el-menu-item>
+          </el-menu-item-group>
+          <el-menu-item-group title="分组2">
+            <el-menu-item index="1-3">选项3</el-menu-item>
+          </el-menu-item-group>
+          <el-submenu index="1-4">
+            <template slot="title">选项4</template>
+            <el-menu-item index="1-4-1">选项1</el-menu-item>
+          </el-submenu>
+        </el-submenu>
+        <el-menu-item index="2">
+          <i class="el-icon-menu"></i>
+          <span slot="title">导航二</span>
+        </el-menu-item>
+        <el-menu-item index="3" disabled>
+          <i class="el-icon-document"></i>
+          <span slot="title">导航三</span>
+        </el-menu-item>
+        <el-menu-item index="4">
+          <i class="el-icon-setting"></i>
+          <span slot="title">导航四</span>
+        </el-menu-item>
+      </el-menu>
     </div>
+    <div class="content">
+      <div class="sub-main" :style="`margin-left:${!isMenu?'0px':'240px'}`">
+        <div class="nav-top">
+          <div class="change-menu">
+            <i class="el-icon-menu" @click="isMenu=!isMenu"></i>
+          </div>
+        </div>
+        <div class="con" :style="`left:${!isMenu?'0px':'240px'}`">
+          content
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -122,14 +122,20 @@ export default {
         background-color: #545c64;
         .change-menu {
           padding-top: 7px;
-          cursor: pointer;
           .el-icon-menu {
+            cursor: pointer;
             display: initial;
             font-size: 30px;
             color: #fff;
           }
         }
       }
+    }
+    .con {
+      position: absolute;
+      top: 60px;
+      bottom: 0;
+      right: 0;
     }
   }
 }
